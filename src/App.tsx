5d4794f7b289f1773bd64fc8e23f3dc28adbba00
Lucas <29230467+Somfic@users.noreply.github.com>
2022-08-31 21:52:25 +0200

    Installed SASS

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
 import { Editor } from "./Editor";
 import { RecoilRoot } from "recoil";
 
 function App() {
-	return (
-		<RecoilRoot>
-			<div className="App">
-				<Editor />
-			</div>
-		</RecoilRoot>
-	);
+    return (
+        <RecoilRoot>
+            <div className="App">
+                <Editor />
+            </div>
+        </RecoilRoot>
+    );
 }
 
 export default App;
