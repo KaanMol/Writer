3504309b15814259c7f0e47903f6f8278da584ec
Kaan <kaanmol@outlook.com>
2022-08-31 22:43:21 +0200

    Values get shared to CoreAPI

--- src/component/CheckboxComponent.tsx
+++ src/component/CheckboxComponent.tsx
@@ -1,16 +1,28 @@
 import { useState } from "react";
 import { CoreAPI } from "../core";
 
-export function CheckboxComponent(props: { api: CoreAPI<{ checked: boolean; text: string }> }) {
-    const [checked, setCheck] = useState(props.api.value.checked);
-    const [text, setText] = useState(props.api.value.text);
+export function CheckboxComponent(props: {
+	api: CoreAPI<{ checked: boolean; text: string }>;
+}) {
+	const [checked, setCheck] = useState(props.api.value.checked);
+	const [text, setText] = useState(props.api.value.text);
 
-    return (
-        <>
-            <input type="checkbox" id={`checkbox${props.api.id}`} name={`checkbox${props.api.id}`} checked={checked as boolean} onChange={(e) => setCheck(e.target.checked)} />
-            <label contentEditable="false" htmlFor={`checkbox${props.api.id}`}>
-                {text}
-            </label>
-        </>
-    );
+	function onCheckboxChange(e: any) {
+		const newValue = e.target.checked;
+		props.api.setValue({ checked: newValue, text: text });
+		setCheck(newValue);
+	}
+
+	return (
+		<>
+			<input
+				type="checkbox"
+				id={`checkbox${props.api.id}`}
+				name={`checkbox${props.api.id}`}
+				checked={checked as boolean}
+				onChange={onCheckboxChange}
+			/>
+			<label htmlFor={`checkbox${props.api.id}`}>{text}</label>
+		</>
+	);
 }
