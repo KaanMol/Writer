613e8f40da64f8a0e7826d3241d0c5d69bc11cde
Kaan <kaanmol@outlook.com>
2022-08-31 10:53:43 +0200

    Cleaned code component a bit up

--- src/component/CodeComponent.tsx
+++ src/component/CodeComponent.tsx
@@ -1,26 +1,28 @@
 import { useState } from "react";
 import { CoreAPI } from "../core";
-import SyntaxHighlighter from 'react-syntax-highlighter';
+import SyntaxHighlighter from "react-syntax-highlighter";
 
-export function CodeComponent(props: { api: CoreAPI<{ language: string, code: string }> }) {
-	const [text, setText] = useState(props.api.value.code);
+export function CodeComponent(props: {
+	api: CoreAPI<{ language: string; code: string }>;
+}) {
+	const [code, setCode] = useState(props.api.value.code);
 	const [language, setLanguage] = useState(props.api.value.language);
 
-    function onChange(e: any) {
-        const newValue = e.target.value;
-        setText(newValue);
-    }
+	return (
+		<>
+			<input
+				contentEditable="false"
+				placeholder="Language"
+				onChange={(e) => setLanguage(e.target.value)}
+			/>
+			<input
+				contentEditable="false"
+				onChange={(e) => setCode(e.target.value)}
+			/>
 
-    function onLanguageChange(e: any) {
-        const newValue = e.target.value;
-        setLanguage(newValue);
-    }
-
-	return <>
-        <input contentEditable="false" placeholder="Language" onChange={onLanguageChange}/>
-        <input contentEditable="false" onChange={onChange}/>
-        <SyntaxHighlighter contentEditable="false" language={language}>
-            {text}
-        </SyntaxHighlighter>
-    </>;
+			<SyntaxHighlighter contentEditable="false" language={language}>
+				{code}
+			</SyntaxHighlighter>
+		</>
+	);
 }
