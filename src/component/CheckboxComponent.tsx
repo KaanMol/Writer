63e08795ebc584905fbb1b65425bc8e8782c9ab7
Lucas <29230467+Somfic@users.noreply.github.com>
2022-08-31 21:42:53 +0200

    Fixed being able to edit label of CheckboxComponent

--- src/component/CheckboxComponent.tsx
+++ src/component/CheckboxComponent.tsx
@@ -1,22 +1,16 @@
 import { useState } from "react";
 import { CoreAPI } from "../core";
 
-export function CheckboxComponent(props: {
-	api: CoreAPI<{ checked: boolean; text: string }>;
-}) {
-	const [checked, setCheck] = useState(props.api.value.checked);
-	const [text, setText] = useState(props.api.value.text);
+export function CheckboxComponent(props: { api: CoreAPI<{ checked: boolean; text: string }> }) {
+    const [checked, setCheck] = useState(props.api.value.checked);
+    const [text, setText] = useState(props.api.value.text);
 
-	return (
-		<>
-			<input
-				type="checkbox"
-				id={`checkbox${props.api.id}`}
-				name={`checkbox${props.api.id}`}
-				checked={checked as boolean}
-				onChange={(e) => setCheck(e.target.checked)}
-			/>
-			<label htmlFor={`checkbox${props.api.id}`}>{text}</label>
-		</>
-	);
+    return (
+        <>
+            <input type="checkbox" id={`checkbox${props.api.id}`} name={`checkbox${props.api.id}`} checked={checked as boolean} onChange={(e) => setCheck(e.target.checked)} />
+            <label contentEditable="false" htmlFor={`checkbox${props.api.id}`}>
+                {text}
+            </label>
+        </>
+    );
 }
